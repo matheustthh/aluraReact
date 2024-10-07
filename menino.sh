20e3f4d4fc38dd022ad1cc3b84734073aca5cf7c
matheustthh <matheus.theodoro.silva@escola.pr.gov.br>
2024-10-07 11:38:14 -0300

    alura project

---
+++ menino.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Qual seu nome menino?"
+
+read name
+
+while true
+do
+   echo "$name é um MENINO bem gay"
+   sleep 1
+done
+
---
+++ meninoGay.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Qual seu nome menino?"
+
+read name
+
+for (( i=0; i<3; i++ ))
+do
+   echo "$name é um MENINO bem gay"
+   sleep 0.2
+done
